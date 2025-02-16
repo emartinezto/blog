d0228d7c6aea7d8d1e7b9fe3f00dac2906f40447
emartinezto <emartineztores@gmail.com>
2025-02-16 13:02:39 +0100

    feat: add component post-form & post-list / add interfaces IPost / clean app.commponent & add new component in app.compoents / add reset css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PostListComponent } from "./components/post-list/post-list.component";
+import { PostFormComponent } from "./components/post-form/post-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PostListComponent, PostFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

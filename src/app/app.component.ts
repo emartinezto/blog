49d439362b71dda076d43496980f8ee0075e161a
emartinezto <emartineztores@gmail.com>
2025-02-22 08:37:58 +0100

    feat: add header&footer components and layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { PostListComponent } from "./components/post-list/post-list.component";
 import { PostFormComponent } from "./components/post-form/post-form.component";
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [PostListComponent, PostFormComponent],
+  imports: [PostListComponent, PostFormComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
